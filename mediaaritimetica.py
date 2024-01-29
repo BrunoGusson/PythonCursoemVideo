274c45c8984933d3741f7da57d08724a90316194
BrunoGusson <85442937+BrunoGusson@users.noreply.github.com>
2024-01-29 16:39:16 -0300

    Add files via upload

---
+++ mediaaritimetica.py
@@ -0,0 +1,5 @@
+n1 = float(input('Primeira nota do aluno'))
+n2 = float(input('Segunda nota do aluno'))
+média = (n1+n2)/2
+print('A média entre a primeira nota {} e a segunda nota {} é igual a: {} '.format(n1, n2, média))
+
