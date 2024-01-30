eeda4e8e7b2ca9c622f6c848519c8618b403cd1e
Bruno Gusson <85442937+BrunoGusson@users.noreply.github.com>
2024-01-30 19:19:14 -0300

    Add files via upload

--- conversordemoedas.py
+++ conversordemoedas.py
@@ -1,3 +1,5 @@
 real = int(input('Quanto dinheiro você tem na carteira? R$'))
 dolar = real/4.95
-print('Com R${:.2f} você pode comprar US$ {:.2f}'.format(real,dolar))
+euro = real/5.37
+print('Com R${:.2f} você pode comprar US$ {:.2f} e EUR${:.2F}'.format(real,dolar, euro))
+
