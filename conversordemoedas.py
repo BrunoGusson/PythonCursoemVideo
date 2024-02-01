4289e7e9b4216a08a78bb01912fa5a33fa46cb0d
Bruno Gusson <85442937+BrunoGusson@users.noreply.github.com>
2024-02-01 14:53:29 -0300

    Delete conversordemoedas.py

--- conversordemoedas.py
+++
@@ -1,5 +0,0 @@
-real = int(input('Quanto dinheiro você tem na carteira? R$'))
-dolar = real/4.95
-euro = real/5.37
-print('Com R${:.2f} você pode comprar US$ {:.2f} e EUR${:.2F}'.format(real,dolar, euro))
-
