1123ccce10975f318addc2e4ffb636d8567ff982
Bruno Gusson <85442937+BrunoGusson@users.noreply.github.com>
2024-02-07 14:51:42 -0300

    Add files via upload

---
+++ reajustesalarial.py
@@ -0,0 +1,3 @@
+sal = float(input('Qual o salário do funcionário? R$'))
+new = sal + (sal*15/100)
+print('O salário do funcionário que costumava ser {} com o reajuste passou a ser {}'.format(sal,new))
