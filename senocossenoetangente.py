8430e6aaba38e046e5a1bc6d6e1700b0ed0b38eb
Bruno Gusson <85442937+BrunoGusson@users.noreply.github.com>
2024-03-14 17:14:33 -0300

    Update senocossenoetangente.py

--- senocossenoetangente.py
+++ senocossenoetangente.py
@@ -2,3 +2,5 @@ import math
 an = float(input('Digite o ângulo que você deseja:'))
 seno = math.sin(math.radians(an))
 print('O ângulo de {} tem o SENO de {:.2f}'.format(an, seno))
+tangente = math.tan(math.radians(an))
+print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(an,tangente))
